6a035077c35c381c378881ebcffbf95b25245b2c
pkini2002 <gpkini2002@gmail.com>
2023-09-23 18:50:37 +0530

    Initial Commit

---
+++ src/main/java/com/example/JWTAuthenticationSpringboot/JwtAuthenticationSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWTAuthenticationSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JWTAuthenticationSpringboot/JwtAuthenticationSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JWTAuthenticationSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
