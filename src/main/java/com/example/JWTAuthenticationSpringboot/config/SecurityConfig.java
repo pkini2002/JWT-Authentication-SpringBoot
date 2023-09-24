b9dbc24af39ce6cad5ff4aa5e0dd42f154a4d9bd
pkini2002 <gpkini2002@gmail.com>
2023-09-24 12:59:58 +0530

    fix: Bean config

--- src/main/java/com/example/JWTAuthenticationSpringboot/config/SecurityConfig.java
+++ src/main/java/com/example/JWTAuthenticationSpringboot/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.JWTAuthenticationSpringboot.config;
 import com.example.JWTAuthenticationSpringboot.security.JWTAthenticationEntryPoint;
 import com.example.JWTAuthenticationSpringboot.security.JWTAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ public class SecurityConfig {
     @Autowired
     private JWTAuthenticationFilter filter;
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // configuration
         http.csrf(csrf->csrf.disable())
